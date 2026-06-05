0bb3f9f49682511cd38b9912c5428bf2a1df4cff
David Santana <davidsantana06@hotmail.com>
2026-06-05 13:44:53 -0300

    feat: update platforms

--- assets/js/content/platforms.js
+++ assets/js/content/platforms.js
@@ -3,11 +3,19 @@ export const platforms = [
     faIcon: "fab fa-linkedin",
     label: "LinkedIn",
     url: "https://linkedin.com/in/davidsantana06/",
+    color: "#0A66C2",
   },
   {
     faIcon: "fab fa-github",
     label: "GitHub",
     url: "https://github.com/davidsantana06",
+    color: "#181717",
+  },
+  {
+    faIcon: "fab fa-youtube",
+    label: "YouTube",
+    url: "https://youtube.com/@davidsantana06",
+    color: "#FF0000",
   },
 ];
 export default platforms;
